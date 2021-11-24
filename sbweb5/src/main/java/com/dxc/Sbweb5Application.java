2d42495ce18e470d08607d7f835e5ae9f5d082f6
ndhanemkula <ndhanemkula@host.docker.internal>
2021-11-23 20:52:23 -0800

    frist commit

---
+++ sbweb5/src/main/java/com/dxc/Sbweb5Application.java
@@ -0,0 +1,13 @@
+package com.dxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbweb5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbweb5Application.class, args);
+	}
+
+}
